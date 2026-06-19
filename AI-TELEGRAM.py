4ffd121715fb8929bf656d1ecdb351bf58b2368a
amir-bot-sourse <a67354343@gmail.com>
2026-06-19 09:18:35 +0330

    Update AI-TELEGRAM.py

--- AI-TELEGRAM.py
+++ AI-TELEGRAM.py
@@ -147,7 +147,7 @@ async def panel(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
     async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-    query = update.callback_query
+        query = update.callback_query
         await query.answer()
 
     user_id = query.from_user.id
