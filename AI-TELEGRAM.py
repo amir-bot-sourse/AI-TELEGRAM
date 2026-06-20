73dd5353924825c7df308250e9d277da522f6c72
amir-bot-sourse <a67354343@gmail.com>
2026-06-20 08:39:48 +0330

    Update AI-TELEGRAM.py

--- AI-TELEGRAM.py
+++ AI-TELEGRAM.py
@@ -332,7 +332,6 @@ async def users_list(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(txt[:4000])
  
     async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
-async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     save_user(update.effective_user.id)
 
