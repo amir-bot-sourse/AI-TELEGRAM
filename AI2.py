f215dfc389ba2ecec2486fad2e5437e5db595ef9
amir-bot-sourse <a67354343@gmail.com>
2026-06-23 16:15:20 +0330

    Update AI2.py

--- AI2.py
+++ AI2.py
@@ -412,11 +412,9 @@ def webhook():
     data = request.get_json(force=True)
     update = Update.de_json(data, app.bot)
 
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
+  loop = asyncio.new_event_loop()
   asyncio.set_event_loop(loop)
-
-loop.run_until_complete(app.process_update(update))
+  loop.run_until_complete(app.process_update(update))
 
     return "ok"
 
