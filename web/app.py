871ee72b253cd1cd88c1f7cbbe3de0f108550c75
amir-bot-sourse <a67354343@gmail.com>
2026-07-29 17:05:08 +0330

    Fix webhook asyncio loop

--- web/app.py
+++ web/app.py
@@ -1,20 +1,20 @@
-from config import BOT_TOKEN
 from flask import Flask, request
 from telegram import Update
 
 import asyncio
 
+from config import BOT_TOKEN
 from bot import application
 
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def home():
-
     return "AI Telegram Bot Running"
 
-@app.route("/webhook", methods=["POST"])
+
 @app.route(f"/{BOT_TOKEN}", methods=["POST"])
 def webhook():
 
@@ -25,7 +25,7 @@ def webhook():
         application.bot
     )
 
-    loop.run_until_complete(
+    asyncio.run(
         application.process_update(update)
     )
 
